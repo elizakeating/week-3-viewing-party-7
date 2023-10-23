3e3e8caee40df4f62180184957abbf56a020ecff
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-23 15:59:23 -0600

    feat: add case sensitive check to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User <ApplicationRecord 
   validates_presence_of :email, :name 
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :password
 
   has_secure_password

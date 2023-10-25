57b90bf9f0e869e5f7ee1651bf30e61c19fa3749
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-24 20:46:16 -0600

    feat: part 2 user story 1

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -22,9 +22,17 @@ RSpec.describe 'Landing Page' do
     expect(current_path).to eq(root_path)
   end 
 
-  it 'lists out existing users' do 
-    user1 = User.create(name: "User One", email: "user1@test.com")
-    user2 = User.create(name: "User Two", email: "user2@test.com")
+  it 'lists out existing users if you are logged in' do 
+    user1 = User.create(name: "User One", email: "user1@test.com", password: "1234")
+    user2 = User.create(name: "User Two", email: "user2@test.com", password: "5678")
+
+    visit login_path
+
+    fill_in :email, with: user1.email
+    fill_in :password, with: user1.password
+    click_button "Log In"
+    
+    visit root_path
 
     expect(page).to have_content('Existing Users:')
 
@@ -40,5 +48,16 @@ RSpec.describe 'Landing Page' do
     click_link "Log In"
 
     expect(current_path).to eq("/login")
+  end
+
+  it "i should not see the section of the page that lists users if i am a visitor" do
+    user1 = User.create(name: "User One", email: "user1@test.com")
+    user2 = User.create(name: "User Two", email: "user2@test.com")
+
+    visit root_path
+
+    expect(page).to_not have_content("Existing Users:")
+    expect(page).to_not have_content(user1.email)
+    expect(page).to_not have_content(user2.email)
   end
 end
